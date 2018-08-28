68860004dc73a97cccb344ec832b38f492bb74fa
Mawanda <giovanni.cavallin@outlook.com>
2018-08-28 16:09:18 +0200

    Added numpy compatibility; fixed last Python27 compatibilities

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 from setuptools import setup
 
 setup(
-    name="alyn",
-    version="0.1.1",
-    author="Kakul Chandra",
-    description="Fix skew in images",
-    author_email="kakulchandra911@gmail.com",
-    url='https://github.com/kakul/Alyn.git',
-    download_url='https://github.com',
-    keywords=['image-processing', 'image-deskew', 'deskew', 'rotate', 'text'],
-    packages=['alyn'],
-    classifiers=[],
-    license='MIT',
-    install_requires=['numpy', 'scikit-image', 'scipy', 'matplotlib'])
+	name="alyn",
+	version="0.1.2",
+	author="Giovanni Cavallin",
+	description="Fix skew in images",
+	author_email="giovanni.cavallin@outlook.com",
+	url='https://github.com/mawanda-jun/Alyn',
+	download_url='https://github.com/mawanda-jun/Alyn.git',
+	keywords=['image-processing', 'image-deskew', 'deskew', 'rotate', 'text'],
+	packages=['alyn'],
+	classifiers=[],
+	license='MIT',
+	install_requires=['numpy', 'scikit-image', 'scipy', 'matplotlib']
+)
